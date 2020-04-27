bf8fa942705bf618ace742f7cf4b72a882baf320
Maximbeshliu <maximbeshliu@yahoo.com>
2020-04-27 21:07:37 +0300

    hw-15

---
+++ script.js
@@ -0,0 +1,58 @@
+//Task 1
+for (let i = 2; i <= 10; i++) {
+  if (i % 2 == 1) continue;
+  console.log(i);
+}
+
+/*Task 2
+
+let i = 0;
+while (i < 3) {
+  alert(`number ${i}!`);
+  i++;
+}
+*/
+
+/*Task 3
+
+let n = 1;
+for (i = 1; i <= 5; i++) {
+  n *= i;
+  console.log(n);
+}
+*/
+
+/*Task 4
+
+let a = +prompt("a?", "");
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert("2,3");
+    break;
+}
+*/
+
+/* Task 5
+
+let browser = prompt("what's your browser?");
+if (browser === "Edge") {
+  alert("You've got the Edge!");
+} else if (browser === "Chrome") {
+  alert("Okay, we supprot this browsers too");
+} else if (browser === "Opera") {
+  alert("Okay, we supprot this browsers too");
+} else if (browser === "Firefox") {
+  alert("Okay, we supprot this browsers too");
+} else if (browser === "Safari") {
+  alert("Okay, we supprot this browsers too");
+} else {
+  alert("We hope that this page looks ok!");
+}
+*/
